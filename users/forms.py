1c28914d2ede1859be90a3f98cb29bb252ae583a
Dani <road2product@gmail.com>
2020-10-03 10:18:24 +0200

    Articl Blog by python

---
+++ users/forms.py
@@ -0,0 +1,20 @@
+from django import  forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+
+
+class CustomUserCreationForm(UserCreationForm.Meta):
+    
+    class Meta:
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields
+
+
+
+class CustomUserChangeForm(UserChangeForm):
+    
+    class Meta:
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields
+
